65a7bca772e3ed97bb1af475c9cac380bd44ea85
Debashis121212 <139324072+Debashis121212@users.noreply.github.com>
2024-02-18 07:33:44 +0000

    Pending changes exported from your codespace

--- XeonCheems10.js
+++ XeonCheems10.js
@@ -7,7 +7,7 @@
 //WhatsApp: +916909137213
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 
-require('./lib/listmenu')
+require('./lib/')
 const {
     BufferJSON,
     WA_DEFAULT_EPHEMERAL,
